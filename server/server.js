5147d088d6c496827693885af254a64a1acb66cb
Andy Park <andyoliverpark@yahoo.com>
2026-07-13 15:38:12 -0700

    Added main components for React Client Application for Client side Design and Handling

--- server/server.js
+++ server/server.js
@@ -6,11 +6,11 @@ import records from "./routes/record.js";
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.arguments(cors());
+app.use(cors());
 app.use(express.json());
 app.use("/record", records);
 
 // STARTS the Express Server
 app.listen(PORT, () => {
-    console.log('Server listening on port ${PORT}');
+    console.log(`Server listening on port: ${PORT}`);
 }); 
